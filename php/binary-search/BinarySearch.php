a8625757368b72c21c766f202528b1454a1808e3
imajinyun <imajinyun@gmail.com>
2020-01-25 22:49:09 +0800

    Update binary-search with php

--- php/binary-search/BinarySearch.php
+++ php/binary-search/BinarySearch.php
@@ -2,6 +2,14 @@
 
 class BinarySearch
 {
+    /**
+     * 二分查找元素在数组中的位置。
+     *
+     * @param int $value 待查找的元素。
+     * @param array $array 待查找的数组。
+     *
+     * @return int -1 表示没有找到元素，否则为元素在数组中的索引。
+     */
     public static function search(int $value, array $array): int
     {
         $low = 0;
