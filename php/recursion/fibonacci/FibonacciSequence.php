474cef1cde6726ecd674c3c405327d76634151bf
imajinyun <imajinyun@gmail.com>
2020-01-26 14:55:18 +0800

    Add fibonacci-sequence with php

---
+++ php/recursion/fibonacci/FibonacciSequence.php
@@ -0,0 +1,24 @@
+<?php
+
+class FibonacciSequence
+{
+    /**
+     * 求给定数字的斐波那契数列值。
+     *
+     * @param int $number 给定的数字。
+     *
+     * @return int 返回 -1 表示计算失败，否则表示计算成功。
+     */
+    public static function fibonacci(int $number): int
+    {
+        if ($number < 0) {
+            return -1;
+        }
+
+        if ($number === 0 || $number === 1) {
+            return $number;
+        }
+
+        return self::fibonacci($number - 1) + self::fibonacci($number - 2);
+    }
+}
