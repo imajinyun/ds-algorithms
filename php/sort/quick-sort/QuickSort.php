477e5f9124d8d92cd457631bfe380a73363a5dcb
imajinyun <imajinyun@gmail.com>
2020-01-26 17:05:21 +0800

    Add quick-sort with php

---
+++ php/sort/quick-sort/QuickSort.php
@@ -0,0 +1,31 @@
+<?php
+
+class QuickSort
+{
+    /**
+     * 快速排序。
+     *
+     * @param array $array 待排序数组。
+     *
+     * @return array
+     */
+    public static function sort(array $array): array
+    {
+        $count = count($array);
+        if ($count < 2) {
+            return $array;
+        }
+
+        [$left, $pivot, $right] = [[], $array[0], []];
+        for ($i = 1; $i < $count; $i++) {
+            $value = $array[$i];
+            if ($value <= $pivot) {
+                $left[] = $value;
+            } else {
+                $right[] = $value;
+            }
+        }
+
+        return array_merge(self::sort($left), [$pivot], self::sort($right));
+    }
+}
