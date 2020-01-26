9b216c6c50f1b24e299e2c7441dc803e34bc0ddb
imajinyun <imajinyun@gmail.com>
2020-01-26 15:28:08 +0800

    Add factorial with php

---
+++ php/recursion/factorial/Factorial.php
@@ -0,0 +1,24 @@
+<?php
+
+class Factorial
+{
+    /**
+     * 求给定数字的阶乘值。
+     *
+     * @param int $number 给定的数字。
+     *
+     * @return int 返回 -1 表示计算失败，否则表示计算成功。
+     */
+    public static function fact(int $number): int
+    {
+        if ($number < 0) {
+            return -1;
+        }
+
+        if ($number === 1) {
+            return $number;
+        }
+
+        return $number * self::fact($number - 1);
+    }
+}
