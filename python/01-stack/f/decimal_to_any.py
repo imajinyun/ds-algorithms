820fe3ca89519d423beec040a64459aab7262f1c
imajinyun <imajinyun@gmail.com>
2020-06-02 11:01:27 +0800

    Add decimal-to-any with python

---
+++ python/01-stack/f/decimal_to_any.py
@@ -0,0 +1,25 @@
+from a.stack import Stack
+
+
+def decimal_to_any(num: int, base: int):
+    """将十进制数转换成任意进制数"""
+    stack, digits = Stack(), '0123456789ABCDEF'
+
+    while num > 0:
+        rem = num % base
+        stack.push(rem)
+        num //= base
+
+    result = ''
+    while not stack.isEmpty():
+        result += str(digits[stack.pop()])
+
+    if base == 16:
+        flag = False
+        for i in result:
+            if i not in 'ABCDEF':
+                flag = True
+                break
+
+        return int(result) if flag else (result if result else 0)
+    return int(result) if result else 0
---
+++ python/01-stack/f/test_decimal_to_any.py
@@ -0,0 +1,31 @@
+import unittest
+
+from decimal_to_any import decimal_to_any
+
+
+class TestDecimalToAny(unittest.TestCase):
+
+    def testDecimalToAny(self):
+        self.assertEqual(0, decimal_to_any(0, 2))
+        self.assertEqual(1, decimal_to_any(1, 2))
+        self.assertEqual(10, decimal_to_any(2, 2))
+        self.assertEqual(1000, decimal_to_any(8, 2))
+
+        self.assertEqual(0, decimal_to_any(0, 10))
+        self.assertEqual(1, decimal_to_any(1, 10))
+        self.assertEqual(10, decimal_to_any(10, 10))
+        self.assertEqual(1000, decimal_to_any(1000, 10))
+
+        self.assertEqual(0, decimal_to_any(0, 8))
+        self.assertEqual(1, decimal_to_any(1, 8))
+        self.assertEqual(12, decimal_to_any(10, 8))
+        self.assertEqual(1750, decimal_to_any(1000, 8))
+
+        self.assertEqual(0, decimal_to_any(0, 16))
+        self.assertEqual(1, decimal_to_any(1, 16))
+        self.assertEqual('A', decimal_to_any(10, 16))
+        self.assertEqual('F', decimal_to_any(15, 16))
+
+
+if __name__ == '__main__':
+    unittest.main()
